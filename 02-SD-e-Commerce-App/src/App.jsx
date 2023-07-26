f351e7d476c7ae60301266a963ca7967add4d27f
Ahmad H <mah07308@gmail.com>
2023-07-27 01:50:43 +0530

    Completed routing using react router v6

--- 02-SD-e-Commerce-App/src/App.jsx
+++ 02-SD-e-Commerce-App/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Home from './Home';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 
 const App = () => {
   return (
     <div className='App'>
       <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/checkout' element={<Checkout />} />
+      </Routes>
     </div>
   );
 };
